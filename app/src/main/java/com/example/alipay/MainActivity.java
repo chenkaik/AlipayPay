e667e50a4dfb84b385db4b75e0d4d154340c8b76
chenkai <chenkai_k7@163.com>
2018-01-21 18:43:58 +0800

    封装代码

--- app/src/main/java/com/example/alipay/MainActivity.java
+++ app/src/main/java/com/example/alipay/MainActivity.java
@@ -104,4 +104,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
+    /**
+     * //签名在客户端
+     findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+    //假设已经从服务端获取必要数据
+    MyALipayUtils.ALiPayBuilder builder = new MyALipayUtils.ALiPayBuilder();
+    builder.setAppid("123")
+    .setRsa("456")//根据情况设置Rsa2与Rsa
+    .setMoney("0.01")//单位时分
+    .setTitle("支付测试")
+    .setOrderTradeId("487456")//从服务端获取
+    .setNotifyUrl("fdsfasdf")//从服务端获取
+    .build()
+    .toALiPay(MainActivity.this);
+    }
+    });
+     //签名在服务端
+     findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+    //假设已从服务端获取orderInfo
+    String orderInfo = "";
+    MyALipayUtils.ALiPayBuilder builder = new MyALipayUtils.ALiPayBuilder();
+    builder.build().toALiPay(MainActivity.this, orderInfo);
+
+    }
+    });
+     */
+
 }
